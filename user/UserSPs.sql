6b87a5d16ebf414447b17840279b33aebed6948b
Sebastian Obando Paniagua <60985278+sebas2002cr@users.noreply.github.com>
2022-06-10 00:23:52 -0600

    Store procedures for creating a product

--- user/UserSPs.sql
+++ user/UserSPs.sql
@@ -80,3 +80,150 @@ BEGIN
 
 END
 GO
+
+-- -------------------------
+
+---Store Procedure, find product by name 
+
+
+CREATE PROCEDURE prcFindProductByName
+@name varchar(50)
+AS
+BEGIN
+	BEGIN TRY 
+		SELECT (		
+			SELECT idProduct, idType, name 
+        	FROM 
+				EXECUTE ('SELECT * FROM product.product') AT [UNIVERSAL-MYSQL] 
+        	WHERE name = @name
+			FOR JSON AUTO
+		) AS productFound
+
+	END TRY 
+	BEGIN CATCH
+	SELECT
+	  ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;
+
+	END CATCH
+
+END
+GO
+
+
+-- Insert productds on US stores (US_User)
+CREATE PROCEDURE prcCreateProductUS
+@idProduct int,
+AS
+BEGIN
+	BEGIN TRY 
+
+		INSERT INTO [dbo].[usa_store1].[product] (idProduct)
+		VALUES (@idProduct);
+		INSERT INTO [dbo].[usa_store2].[product] (idProduct)
+		VALUES (@idProduct);
+		INSERT INTO [dbo].[usa_store3].[product] (idProduct)
+		VALUES (@idProduct);
+	END TRY 
+	BEGIN CATCH
+	SELECT
+	  ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;
+
+	END CATCH
+
+END
+GO
+
+
+-- Insert products on STK stores (STK_User)
+CREATE PROCEDURE prcCreateProductSTK
+@idProduct int,
+AS
+BEGIN
+	BEGIN TRY 
+
+		INSERT INTO [dbo].[stk_store1].[product] (idProduct)
+		VALUES (@idProduct);
+		INSERT INTO [dbo].[stk_store2].[product] (idProduct)
+		VALUES (@idProduct);
+		INSERT INTO [dbo].[stk_store3].[product] (idProduct)
+		VALUES (@idProduct);
+	END TRY 
+	BEGIN CATCH
+	SELECT
+	  ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;
+
+	END CATCH
+
+END
+GO
+
+-- Store procedure to get next Product ID (User)
+CREATE PROCEDURE prcGetNextProductId
+AS
+BEGIN
+	BEGIN TRY 
+		Execute ('SELECT MAX(idProduct) as maxIDuser
+		FROM product.product') AT [UNIVERSAL-MYSQL] 
+	END TRY 
+	BEGIN CATCH
+	SELECT
+	  ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;
+
+	END CATCH
+
+END
+GO
+
+
+
+
+-- Insert products on IE stores (IE_User)
+CREATE PROCEDURE prcCreateProductIE
+@idProduct int,
+AS
+BEGIN
+	BEGIN TRY 
+
+		INSERT INTO [dbo].[ie_store1].[product] (idProduct)
+		VALUES (@idProduct);
+		INSERT INTO [dbo].[ie_store2].[product] (idProduct)
+		VALUES (@idProduct);
+		INSERT INTO [dbo].[ie_store3].[product] (idProduct)
+		VALUES (@idProduct);
+	END TRY 
+	BEGIN CATCH
+	SELECT
+	  ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;
+
+	END CATCH
+
+END
+GO
+
+
+
