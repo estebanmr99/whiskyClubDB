e18a2f6a92527ffd323c88723565e2b41872794a
Sebastian Obando Paniagua <60985278+sebas2002cr@users.noreply.github.com>
2022-06-10 21:36:50 -0600

    tables for product

--- master/master.sql
+++ master/master.sql
@@ -2,3 +2,6 @@ CREATE DATABASE masterdb;
 
 CREATE TABLE tempNextUserId (maxIDuser INT);
 CREATE TABLE tempUserJson (userFound VARCHAR(MAX));
+
+CREATE TABLE tempNextProductId (maxIDuser INT);
+CREATE TABLE tempProductJson (userFound VARCHAR(MAX));
